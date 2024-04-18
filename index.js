88b78e92b409c1b7a8fe04e51628aaf7c8100dfa
geraldkisilu <muligerald6@gmail.com>
2024-04-18 14:57:41 +0300

    commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// Variables
+const viewPromosBtn = document.getElementById('view-promos-btn');
+const viewEventsBtn = document.getElementById('view-events-btn');
+const viewMapBtn = document.getElementById('view-map-btn');
+
+// Functions
+function viewPromotions() {
+    // Logic to display promotions
+    console.log('Displaying promotions...');
+}
+
+function viewEvents() {
+    // Logic to display events
+    console.log('Displaying events...');
+}
+
+function viewMap() {
+    // Logic to display mall map
+    console.log('Displaying mall map...');
+}
+
+// Event Listeners
+viewPromosBtn.addEventListener('click', viewPromotions);
+viewEventsBtn.addEventListener('click', viewEvents);
+viewMapBtn.addEventListener('click', viewMap);
